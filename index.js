f1c1149b3c44c17c5e5ccd310d18536f3ee712df
Jeffery Bennett <meandmybadself@gmail.com>
2016-12-10 21:15:37 -0600

    Adding source docs.

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ if (!process.env.BRYANT_USER) {
   require('dotenv').config()
 }
 
-let BryantController = require('./lib/bryant-controller/index.js');
-let b = new BryantController(process.env.BRYANT_USER, process.env.BRYANT_PASSWORD);
+let BryantController = require('./lib/bryant-controller/index.js')
+let b = new BryantController()
+
+ Promise.all([b.login(process.env.BRYANT_USER, process.env.BRYANT_PASSWORD), b.getLocations()])
+ .then((data) => {
+   console.log('data', data)
+ })
+ .catch((e) => {
+   console.log('error', e)
+ })
+
+// WZ4ZHY8bSu6l%2BClND0v0HGpFLig%3D
+
+console.log(b._generateSignature('https://www.app-api.eng.bryant.com/users/jeffandashley/locations', oauthObj, 'GET'))
