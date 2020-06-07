bc770db3f6c37ae22ee515596cc794ef7d5a05da
Jeffery Bennett <jeffery@permanentrecord.io>
2020-06-07 07:14:31 -0500

    Updates node to latest LTS

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ if (!process.env.BRYANT_USER) {
   require('dotenv').config()
 }
 
-let BryantController = require('./lib/bryant-controller/index.js');
-let b = new BryantController(process.env.BRYANT_USER, process.env.BRYANT_PASSWORD);
+const BryantController = require('./lib/bryant-controller/index.js')
+new BryantController(process.env.BRYANT_USER, process.env.BRYANT_PASSWORD)
